daa7c692a5cf2d5dc9c4b4dc1db1d8322c8ae70e
BethFromAbove <bethcott@hotmail.co.uk>
2020-11-04 19:37:42 +0000

    Playing with 2D sprites and cameras

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float walkingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WalkHandler();
+    }
+
+    void WalkHandler()
+    {
+        //input on x (horiz)
+        float hAxis = Input.GetAxis("Horizontal");
+
+
+        //input on y (vert)
+        float vAxis = Input.GetAxis("Vertical");
+
+        //Movement vector
+        Vector3 movement = new Vector3(hAxis * walkingSpeed * Time.deltaTime, vAxis * walkingSpeed * Time.deltaTime, 0);
+
+        //Calculate new position
+        transform.position = transform.position + movement;
+
+    }
+}
