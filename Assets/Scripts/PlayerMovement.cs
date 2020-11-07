cc442d36ac48792764a30da9a1cc0d116eab95dd
BethFromAbove <bethcott@hotmail.co.uk>
2020-11-07 12:39:58 +0000

    Camera switches views on spacebar press. Player can only move in zoomed in view

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public float walkingSpeed;
+	public Camera cam1;
+	public Camera cam2;
+	private bool zoomedIn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.enabled = true;
+        cam2.enabled = false;
+        zoomedIn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        WalkHandler();
+        if (Input.GetKeyDown("space")) {
+         cam1.enabled = !cam1.enabled;
+         cam2.enabled = !cam2.enabled;
+         zoomedIn = !zoomedIn;
+     	}
+
+     	if (zoomedIn)
+     	{
+     		WalkHandler();
+     	}
     }
 
     void WalkHandler()
     {
         //input on x (horiz)
         float hAxis = Input.GetAxis("Horizontal");
-
 
         //input on y (vert)
         float vAxis = Input.GetAxis("Vertical");
@@ -32,6 +45,5 @@ public class PlayerMovement : MonoBehaviour
 
         //Calculate new position
         transform.position = transform.position + movement;
-
     }
 }
